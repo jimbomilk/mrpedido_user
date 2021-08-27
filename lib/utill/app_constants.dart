24073cf6a74fba85abd9628600648236019d6dc5
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2021-08-27 12:12:20 +0200

    muchos cambios

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -2,7 +2,6 @@ import 'package:flutter_restaurant/data/model/response/language_model.dart';
 import 'package:flutter_restaurant/utill/images.dart';
 
 class AppConstants {
-
   static const String APP_NAME = 'MrPedido';
 
   static const String BASE_URL = 'https://admin.mrpedido.com';
@@ -27,7 +26,8 @@ class AppConstants {
   static const String TOKEN_URI = '/api/v1/customer/cm-firebase-token';
   static const String PLACE_ORDER_URI = '/api/v1/customer/order/place';
   static const String ADDRESS_LIST_URI = '/api/v1/customer/address/list';
-  static const String REMOVE_ADDRESS_URI = '/api/v1/customer/address/delete?address_id=';
+  static const String REMOVE_ADDRESS_URI =
+      '/api/v1/customer/address/delete?address_id=';
   static const String ADD_ADDRESS_URI = '/api/v1/customer/address/add';
   static const String UPDATE_ADDRESS_URI = '/api/v1/customer/address/update/';
   static const String SET_MENU_URI = '/api/v1/products/set-menu';
@@ -36,18 +36,24 @@ class AppConstants {
   static const String COUPON_APPLY_URI = '/api/v1/coupon/apply?code=';
   static const String ORDER_LIST_URI = '/api/v1/customer/order/list';
   static const String ORDER_CANCEL_URI = '/api/v1/customer/order/cancel';
-  static const String UPDATE_METHOD_URI = '/api/v1/customer/order/payment-method';
-  static const String ORDER_DETAILS_URI = '/api/v1/customer/order/details?order_id=';
+  static const String UPDATE_METHOD_URI =
+      '/api/v1/customer/order/payment-method';
+  static const String ORDER_DETAILS_URI =
+      '/api/v1/customer/order/details?order_id=';
   static const String WISH_LIST_GET_URI = '/api/v1/customer/wish-list';
-  static const String ADD_WISH_LIST_URI = '/api/v1/customer/wish-list/add?product_id=';
-  static const String REMOVE_WISH_LIST_URI = '/api/v1/customer/wish-list/remove?product_id=';
+  static const String ADD_WISH_LIST_URI =
+      '/api/v1/customer/wish-list/add?product_id=';
+  static const String REMOVE_WISH_LIST_URI =
+      '/api/v1/customer/wish-list/remove?product_id=';
   static const String NOTIFICATION_URI = '/api/v1/notifications';
   static const String UPDATE_PROFILE_URI = '/api/v1/customer/update-profile';
   static const String SEARCH_URI = '/api/v1/products/search?name=';
   static const String REVIEW_URI = '/api/v1/products/reviews/submit';
   static const String PRODUCT_DETAILS_URI = '/api/v1/products/details/';
-  static const String LAST_LOCATION_URI = '/api/v1/delivery-man/last-location?order_id=';
-  static const String DELIVER_MAN_REVIEW_URI = '/api/v1/delivery-man/reviews/submit';
+  static const String LAST_LOCATION_URI =
+      '/api/v1/delivery-man/last-location?order_id=';
+  static const String DELIVER_MAN_REVIEW_URI =
+      '/api/v1/delivery-man/reviews/submit';
 
   // Shared Key
   static const String THEME = 'theme';
@@ -62,8 +68,20 @@ class AppConstants {
   static const String TOPIC = 'notify';
 
   static List<LanguageModel> languages = [
-    LanguageModel(imageUrl: Images.united_kindom, languageName: 'English', countryCode: 'US', languageCode: 'en'),
-    LanguageModel(imageUrl: Images.arabic, languageName: 'Arabic', countryCode: 'SA', languageCode: 'ar'),
-    LanguageModel(imageUrl: Images.spain, languageName: 'Español', countryCode: 'ES', languageCode: 'es'),
+    LanguageModel(
+        imageUrl: Images.united_kindom,
+        languageName: 'English',
+        countryCode: 'US',
+        languageCode: 'en'),
+    LanguageModel(
+        imageUrl: Images.arabic,
+        languageName: 'Arabic',
+        countryCode: 'SA',
+        languageCode: 'ar'),
+    LanguageModel(
+        imageUrl: Images.spain,
+        languageName: 'Español',
+        countryCode: 'ES',
+        languageCode: 'es'),
   ];
 }
